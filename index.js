9fb5edcf79a281a5e952fd4ab9d28879b2e7a5ec
Rishikesh-kumar-7258 <rishi7258prince@gmail.com>
2022-06-20 11:30:26 +0530

    A rectanble is drawn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 console.log("This is my whiteboard project");
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+console.log(ctx);
+
+canvas.addEventListener("mousemove", (e) => {});
+
+window.addEventListener("click", (e) => {
+  console.log("This is click");
+});
+
+function drawRect(x, y, height, width) {
+  ctx.rect(x, y, width, height);
+  ctx.lineWidth = 1;
+  ctx.strokeStyle = "#FF0000";
+  ctx.fillStyle = "#FFFF00";
+  ctx.stroke();
+  ctx.fill();
+}
+
+drawRect(50, 50, 50, 50);
